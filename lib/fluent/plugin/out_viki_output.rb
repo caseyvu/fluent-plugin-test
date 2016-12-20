7730aa0df6876c16a78eaf524d5f8ae6da5b0bf7
Casey Vu <anhthu.vu@viki.com>
2016-12-20 15:08:44 +0800

    commit

--- lib/fluent/plugin/out_viki_output.rb
+++ lib/fluent/plugin/out_viki_output.rb
@@ -51,7 +51,7 @@ module Fluent
       #          else messages['params']
       #          end
 
-      router.emit('development', time, messages.merge({ ip: ip}))
+      router.emit('development', time, messages))
 
       #status = messages['status']
       #if status == 200
